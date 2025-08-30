53f1a693b0ce737ef49d0aa46c7e4ee1517d7604
Yash Tyagi <contactyashtyagi@gmail.com>
2025-08-30 16:40:37 +0530

    Init funroad

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="flex justify-center w-full h-screen">
+            <h1 className="text-3xl">Funroad - by Yash Tyagi</h1>
+        </div>
+    );
+}
